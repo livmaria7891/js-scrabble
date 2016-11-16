d44568ec5a5797c7f42f3d7d2c97c8156b3bff8d
Olivia <reach.olivialegge@gmail.com>
2016-11-16 14:49:47 -0800

    highestScoreFrom function

--- scrabble.js
+++ scrabble.js
@@ -43,6 +43,31 @@ var Scrabble = function() {
     }
     return score;
   };
+
+  this.highestScoreFrom = function(arrayOfWords){
+    //set highestScore var to 0
+    var highestScore = 0;
+    //set bestWord var to ;
+    var bestWord;
+    var that = this;
+    //iterate through arrayOfWords
+    arrayOfWords.forEach(function(word){
+      //set variable wordScore to equal scoreWord(word)
+      var wordScore = that.scoreWord(word);
+      //if wordScore > highestScore
+      if(wordScore > highestScore){
+        //set bestWord to word
+        bestWord = word;
+        //set highestScore to wordScore
+        highestScore = wordScore;
+        //else if they're equal && word.length < bestWord.length
+      }else if(wordScore == highestScore && word.length < bestWord.length){
+        //set bestWord to word
+        bestWord = word;
+      }
+    });
+    return bestWord;
+  };
 };
 
 // YOUR CODE HERE
@@ -51,8 +76,22 @@ var Scrabble = function() {
 // };
 
 //Manual Tests
-tst1 = new Scrabble();
-tst1.scoreWord("A");
-tst1.scoreWord("patagonia");
+// tst1 = new Scrabble();
+// tst1.scoreWord("A");
+// tst1.scoreWord("patagonia");
+
+// var arrayOfWords = ["oat", "boat", "boats","borzoi"]; //should return borzoi
+// var tiedWordsSameLength = ["tote", "tots"]; //should return tote (because it was first)
+// var tiedWordsDifferentLength = ["dot", "oats"]; // should return dot (because it's shortest)
+//
+// tst2 = new Scrabble();
+// console.log(tst2.highestScoreFrom(arrayOfWords));
+// console.log(tst2.highestScoreFrom(tiedWordsSameLength));
+// console.log(tst2.highestScoreFrom(tiedWordsDifferentLength));
+
+
+
+
+
 
 module.exports = Scrabble;
